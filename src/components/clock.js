c44d5fc801ab60c36af25fc32837eb1acdd6bb35
kiresuah <kiresuah@gmail.com>
2016-07-10 10:29:17 -0400

    changed display time into minutes

--- src/components/clock.js
+++ src/components/clock.js
@@ -8,12 +8,13 @@ export default class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      timer: props.timer,
-      start: props.timer,
+      timer: props.start * 60,
+      start: props.start,
       labelCount: props.labelCount,
       active: false,
-      sessionDuration: '25',
       breakDuration: '5',
+      workDuration: props.start,
+      mode: 'Working',
     };
     this.setTimer = this.setTimer.bind(this);
     this.resetTimer = this.resetTimer.bind(this);
@@ -21,55 +22,63 @@ export default class Clock extends Component {
     this.toggleActive = this.toggleActive.bind(this);
     this.generateLabels = this.generateLabels.bind(this);
     this.breakChange = this.breakChange.bind(this);
-    this.sessionChange = this.sessionChange.bind(this);
+    this.workChange = this.workChange.bind(this);
     this.labelChange = this.labelChange.bind(this);
   }
 
   componentDidMount() {
     // decrement clock timer
-    setInterval(this.decrementTimer, 50);
+    setInterval(this.decrementTimer, 150);
   }
   
-  setTimer(timer) {
-    this.setState({ timer });
+  setTimer(label) {
+    this.setState({ timer: 60 * label });
   }
   
   decrementTimer() {
-    const { active, timer, start } = this.state;
+    const { active, timer, start, mode, breakDuration, workDuration } = this.state;
     if (active && timer > 0) {
-      this.setState({ timer: timer - 0.05 });     
-    } else if(active) {
-      this.setState({ active: false, timer: start });
+      this.setState({ timer: timer - 0.15 });     
+    } else if(active && mode === 'Working') {
+      this.setState({ mode: 'On Break', timer: breakDuration * 60, start: breakDuration });
+    } else if(active && mode === 'On Break') {
+      this.setState({ mode: 'Working', timer: workDuration * 60, start: workDuration });      
     }
   }
   
   resetTimer() {
-    this.setState({ timer: this.state.start, active: false });  
+    this.setState({
+      timer: 60 * this.state.workDuration,
+      active: false,
+      start: this.state.workDuration,
+      mode: 'Working'
+    });  
   }
   
   toggleActive() {
     const { timer, active, start } = this.state;
     this.setState({ active: !active });
-    if (timer <= 0) {
-      this.setState({ timer: start});
-    }
   }
   
-  sessionChange(e) {
+  workChange(e) {
     const val = e.target.value;
-    if (val <= 999) {
+    if (val >= 1 && val <= 999) {
       this.setState({ 
-        sessionDuration: val,
-        start: val,
-        timer: val,
+        workDuration: val,
+        start: this.state.mode === 'Working' ? val : this.state.start,
+        timer: this.state.mode === 'Working' ? 60 * val : this.state.timer,
       });
     }    
   }
   
   breakChange(e) {
     const val = e.target.value;
-    if (val <= 999) {
-      this.setState({ breakDuration: val });
+    if (val >= 1 && val <= 999) {
+      this.setState({ 
+        breakDuration: val,
+        start: this.state.mode === 'On Break' ? val : this.state.start,
+        timer: this.state.mode === 'On Break' ? 60 * val : this.state.timer,
+      });
     }
   }
   
@@ -90,8 +99,15 @@ export default class Clock extends Component {
       const y = radius - radius * Math.sin(radians) - 35;
       radians += clockincrement;
       
+      // adjust the text for case where Math.round would create duplicate labels
+      let text;
+      if (labelCount > start) {
+        text = (start - (i * timeIncrement)).toFixed(1);
+      } else {
+        text = Math.round(start - (i * timeIncrement));
+      }
       const label = (
-        <ClockTick click={this.setTimer} key={i} x={x} y={y} label={Math.round(start - (i * timeIncrement))} />
+        <ClockTick click={this.setTimer} key={i} x={x} y={y} label={text} />
       );
       results.push(label);
     }
@@ -99,9 +115,9 @@ export default class Clock extends Component {
   }
 
   render() {
-    const { start, timer, active, labelCount, sessionDuration, breakDuration } = this.state;
+    const { start, mode, timer, active, labelCount, sessionDuration, breakDuration, workDuration } = this.state;
     const labels = this.generateLabels(225, start, labelCount);
-    const radians = -2 * Math.PI * (timer / start);
+    const radians = -2 * Math.PI * (timer / (60 * start));
     
     const pause = <i className="fa fa-pause" aria-hidden="true"></i>;
     const play = <i className="fa fa-play" aria-hidden="true"></i>;
@@ -113,23 +129,24 @@ export default class Clock extends Component {
       <div className="clock">
         <div id="clock-container" >
         <div id="clock-body" className="shadow">
+          <Hand radians={radians}/>
           <div id="input-container">
             <div id="input-duration-container">
               <label>Session:</label>
-              <input id="session-input" className="form-control user-input" onChange={this.sessionChange} type="text" value={sessionDuration} />
+              <input id="session-input" className="form-control user-input" onChange={this.workChange} mode="text" value={workDuration} />
               <label>Break:</label>
-              <input id="break-input" className="form-control user-input" onChange={this.breakChange} type="text" value={breakDuration} />
+              <input id="break-input" className="form-control user-input" onChange={this.breakChange} mode="text" value={breakDuration} />
             </div>
             <div id="input-config-container">      
-              <label>Tick Count:</label>      
-              <input id="label-input" className="form-control user-input" onChange={this.labelChange} type="text" value={labelCount} />
+              <label>Label Count:</label>      
+              <input id="label-input" className="form-control user-input" onChange={this.labelChange} mode="text" value={labelCount} />
               <div id="start-stop-container" onClick={this.toggleActive} style={playPauseStyle}>{active ? pause : play}</div>
               <div id="reset-container" onClick={this.resetTimer}>{stop}</div>
             </div>
+            <div id="mode-container">{mode}</div>
           </div>
           <div id="clock-labels">{labels}</div>
-          <Hand radians={radians}/>
-          <div id="clock-timer">{Math.round(timer)}</div>
+          <div id="clock-timer">{Math.round(timer / 60)}</div>
         </div>
         </div>
       </div>
